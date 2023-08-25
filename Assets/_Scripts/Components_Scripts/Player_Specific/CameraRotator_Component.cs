c372db507676e5bfc5e72cfdf4ce85a83166d52e
Tyskaro <Tyskaro@hotmail.com>
2023-08-25 11:50:15 -0300

    Third Person Camera
    Adição do script que controla a rotação da câmera dependendo da posição do mouse.

---
+++ Assets/_Scripts/Components_Scripts/Player_Specific/CameraRotator_Component.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator_Component : MonoBehaviour
+{
+    private float xOffset; //Up
+    private float yOffset; //down
+    [SerializeField] private Vector2 offsetMultiplier;
+
+    private void Start()
+    {
+        StartCoroutine(CalculateOffsset());
+        offsetMultiplier *= 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private IEnumerator CalculateOffsset()
+    {
+        yield return new WaitForSeconds(0.2f);
+        while (true)
+        {
+            transform.Rotate(Vector3.up, (Input.GetAxis("Mouse X") * Time.deltaTime) * offsetMultiplier.x, Space.World);
+            if (transform.rotation.eulerAngles.x > 45 && transform.rotation.eulerAngles.x < 55 && Input.GetAxis("Mouse Y") < 0);
+            else if (transform.rotation.eulerAngles.x < 330 && transform.rotation.eulerAngles.x > 300 && Input.GetAxis("Mouse Y") > 0) ;
+            else
+            {
+                transform.Rotate(Vector3.right, (Input.GetAxis("Mouse Y") * Time.deltaTime) * (offsetMultiplier.y * -1), Space.Self);
+            }
+            yield return 0;
+        }
+
+    }
+}
