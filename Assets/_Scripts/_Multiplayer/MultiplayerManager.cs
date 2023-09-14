80b53363f9e5aeaa94d809212c42f8a414a8de90
Tyskaro <Tyskaro@hotmail.com>
2023-09-14 20:34:20 -0300

    Multiplayer Manager

--- Assets/_Scripts/_Multiplayer/MultiplayerManager.cs
+++ Assets/_Scripts/_Multiplayer/MultiplayerManager.cs
@@ -1,48 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Photon.Realtime;
 using Photon.Pun;
 
-public class GameConnection : MonoBehaviourPunCallbacks
+public class MultiplayerManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private string nickName = "Antigen";
     public string roomName = "PUCC";
     void Start()
     {
-        //Conecta no photon
-        Debug.Log("Conectando no Servidor...");
-        PhotonNetwork.NickName = "Xarope " + Random.Range(0, 1000);
+        nickName += Random.Range(0, 10);
+        Debug.Log("Connecting to server...");
+        PhotonNetwork.NickName = nickName;
         PhotonNetwork.ConnectUsingSettings();
     }
-
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
-        Debug.Log("Conectado no Servidor!");
+        Debug.Log("Connected");
         PhotonNetwork.JoinLobby();
     }
     public override void OnJoinedLobby()
     {
         base.OnJoinedLobby();
-        Debug.Log("Entrou no Lobby!");
+        Debug.Log("InLobby");
 
         RoomOptions roomOptions = new RoomOptions();
-        roomOptions.MaxPlayers= 20;
+        roomOptions.MaxPlayers = 4;
 
-        Debug.Log("Entrando na sala "+ roomName);
+        Debug.Log("Entering room " + roomName);
         PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, null);
     }
     public override void OnJoinedRoom()
     {
-        Debug.Log("Entrei na sala " + PhotonNetwork.CurrentRoom.Name);
+        Debug.Log("On room " + PhotonNetwork.CurrentRoom.Name);
         base.OnJoinedRoom();
     }
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        Debug.Log("Player entrou na sala " + newPlayer.NickName);
+        Debug.Log("Player entered " + newPlayer.NickName);
         base.OnPlayerEnteredRoom(newPlayer);
     }
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
-        Debug.Log("Player saiu sala " + otherPlayer.NickName);
+        Debug.Log("Player " + otherPlayer.NickName + " left the room");
         base.OnPlayerLeftRoom(otherPlayer);
     }
 }
