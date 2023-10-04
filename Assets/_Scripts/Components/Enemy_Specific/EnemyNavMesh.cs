063504f605845a01fb0f794ca9eed6bcb462f806
Caosbot <91979537+Caosbot@users.noreply.github.com>
2023-10-04 12:32:34 -0300

    Na _Scenes de test Eduardo
    Objeto se move até um ponto final.

---
+++ Assets/_Scripts/Components/Enemy_Specific/EnemyNavMesh.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavMesh : MonoBehaviour
+{
+    private NavMeshAgent enemyAgent;
+    private Vector3 targetAgent;
+    private Vector3 enemyPosition;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyAgent = GetComponent<NavMeshAgent>();
+        //enemyPosition = GameObject.FindGameObjectWithTag("SpawPoint").transform.position;
+        targetAgent = GameObject.FindGameObjectWithTag("EndPoint").transform.position; 
+        if (targetAgent == null )
+        {
+            Debug.Log("Vazio");
+        }
+        Debug.Log("targetAgent = "+ targetAgent);
+        enemyAgent.speed = 10f;
+        enemyAgent.acceleration = 10f;
+        enemyAgent.stoppingDistance = 0.5f;
+    }
+
+    // Update is called every frame
+    
+    void Update()
+    {
+        
+        enemyPosition = transform.position;
+        distance = Vector3.Distance(targetAgent, enemyPosition);
+        if (distance >=4 )
+        {
+            enemyAgent.SetDestination(targetAgent);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
--- Assets/_Scripts/_Specifis/SpawnSystem/SpawnSystem.cs
+++ Assets/_Scripts/_Specifis/SpawnSystem/SpawnSystem.cs
@@ -29,7 +29,7 @@ public class SpawnSystem : MonoBehaviourPunCallbacks
     }
     private void Start()
     {
-        if (PhotonNetwork.IsMasterClient)
+       // if (PhotonNetwork.IsMasterClient)
         {
             StartCoroutine(SpawnEnemyWaves());
         }
